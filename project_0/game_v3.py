bd00bc8d636ea7de8f9e58b932142c90dbcc366b
Mariya-VB <bogdanova.marya2018@yandex.ru>
2023-02-12 12:00:00 +0800

    Changes the last lines

--- project_0/game_v3.py
+++ project_0/game_v3.py
@@ -2,20 +2,20 @@
 
 import numpy as np
 
-number = np.random.randint(1, 101) # загадываем число
 
-def game_predict(number: int = 1) -> int:
-    """Угадываем число с учетом диапазона
+def random_predict(number: int = 1) -> int:
+    """Угадываем число
+
     Args:
-        number (int, optinal): Загаданное число. Defaults to 1.
+        number (int, optional): Загаданное число. Defaults to 1.
+
     Returns:
         int: Число попыток
     """
-    
     count = 0
     first = 1
-    last = 100
-    
+    last = 101
+
     while True:
         count+=1
         number_predict = (first + last)//2 # выбираем число в середине диапазона
@@ -28,28 +28,30 @@ def game_predict(number: int = 1) -> int:
             
         else:
             first = number_predict # диапазон смещаем вправо
-            
+           
     return count
 
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
 
-def score_game(game_predict) -> int:
-    """За какое количество попыток в среднем за 1000 подходов угадывается число
-    
     Args:
-        game_predict ([type]): функция угадывания
-       
-    Retuns:
+        random_predict ([type]): функция угадывания
+
+    Returns:
         int: среднее количество попыток
     """
     count_ls = []
-    np.random.seed(1)  # фиксируем сид для воспроизводимости
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
-        count_ls.append(game_predict(number))
+        count_ls.append(random_predict(number))
 
     score = int(np.mean(count_ls))
-    print(f'Алгоритм угадывает число в среднем на 1000 подходов за {score} попыток')
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
     return score
 
-score_game(game_predict)
+
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
